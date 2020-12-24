7e4839633f705b478f615195fd343e8f3c98d86c
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2020-12-24 13:02:56 -0500

    pretty

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Page from "../templates/page";
-import { Transition } from 'react-transition-group';
+import { Transition } from "react-transition-group";
 import ScrollAnimation from "react-animate-on-scroll";
 
 // images
@@ -26,86 +26,107 @@ import Dude from "../assets/images/dude.png";
 // scss
 import "../styles/hero-home.scss";
 import "../styles/general.scss";
-import '../styles/home-tracks.scss';
-
-
-
+import "../styles/home-tracks.scss";
 
 // slider
 //Duration of Animation
 const duration = 300;
 
-//Default styles of info container 
+//Default styles of info container
 const defaultStyle = {
   transition: `${duration}ms ease-in-out`,
   transitionProperty: "opacity, right",
   // transitionDelay: '0ms'
   opacity: 0,
   right: 0
-}
+};
 
 //Changing styles depending on state of mount
 const transitionStyles = {
-  entering: {right: -30, opacity: 1, transitionDelay: `${duration}ms` },
-  entered:  {right: 0, opacity: 1 },
-  exiting:  {right: -30, opacity: 0},
-  exited:  {right: -30, opacity: 0},
+  entering: { right: -30, opacity: 1, transitionDelay: `${duration}ms` },
+  entered: { right: 0, opacity: 1 },
+  exiting: { right: -30, opacity: 0 },
+  exited: { right: -30, opacity: 0 }
 };
 
-//Default styles of images 
+//Default styles of images
 const defaultStyleImage = {
   transition: `${duration}ms ease-in-out`,
   transitionProperty: "opacity, right",
   // transitionDelay: '0ms'
   opacity: 0,
   right: 0
-}
+};
 
 //Changing styles depending on state of mount
 const transitionStylesImage = {
-  entering: {right: -30, opacity: 1, transitionDelay: `${duration}ms` },
-  entered:  {right: 0, opacity: 1 },
-  exiting:  {right: -30, opacity: 0},
-  exited:  {right: -30, opacity: 0},
+  entering: { right: -30, opacity: 1, transitionDelay: `${duration}ms` },
+  entered: { right: 0, opacity: 1 },
+  exiting: { right: -30, opacity: 0 },
+  exited: { right: -30, opacity: 0 }
 };
 
-const TrackOne = ({in: inProp}) => (
+const TrackOne = ({ in: inProp }) => (
   //rtg component, in determines when to turn on and off the component
   <Transition in={inProp} timeout={duration}>
     {state => (
       //pass style in with state of mount
-      <div  className="track-infoContainer" style={{
-        ...defaultStyle,
-        ...transitionStyles[state]
-      }}>
-        <h1 className="track-trackHeader">Giving Back To<br />Your <span>Locals</span></h1>
-        <p className="track-trackBody">We here at StartUp want to be able to give back. During the global pandemic, small businesses have been hit the hardest. We want to offer students a second track, the ability to be able to work hands-on with a local small business to Savannah or Atlanta, and find their unique set of problems and what they’d do to solve it.<br/><br/><i>*Judging criteria will be different for each track</i></p>
+      <div
+        className="track-infoContainer"
+        style={{
+          ...defaultStyle,
+          ...transitionStyles[state]
+        }}
+      >
+        <h1 className="track-trackHeader">
+          Giving Back To
+          <br />
+          Your <span>Locals</span>
+        </h1>
+        <p className="track-trackBody">
+          We here at StartUp want to be able to give back. During the global pandemic, small
+          businesses have been hit the hardest. We want to offer students a second track, the
+          ability to be able to work hands-on with a local small business to Savannah or Atlanta,
+          and find their unique set of problems and what they’d do to solve it.
+          <br />
+          <br />
+          <i>*Judging criteria will be different for each track</i>
+        </p>
       </div>
     )}
   </Transition>
-)
+);
 
-const TrackTwo = ({in: inProp}) => (
+const TrackTwo = ({ in: inProp }) => (
   <Transition in={inProp} timeout={duration}>
     {state => (
-      <div className="track-infoContainer" style={{
-        ...defaultStyle,
-        ...transitionStyles[state]
-      }}>
-        <h1 className="track-trackHeader"><span>Entrepreneurship</span><br />The core of StartUp</h1>
-        <p className="track-trackBody">This is the core of StartUp. Students who shoose this track will becoming up with their own, new innovative business ideas about their created product or service. Here, student designers have the most free reign to design a creative solution to the problem we present them with. This has, and will always be, StartUp’s core.<br /><br/><i>*Judging criteria will be different for each track</i></p>
-      
+      <div
+        className="track-infoContainer"
+        style={{
+          ...defaultStyle,
+          ...transitionStyles[state]
+        }}
+      >
+        <h1 className="track-trackHeader">
+          <span>Entrepreneurship</span>
+          <br />
+          The core of StartUp
+        </h1>
+        <p className="track-trackBody">
+          This is the core of StartUp. Students who shoose this track will becoming up with their
+          own, new innovative business ideas about their created product or service. Here, student
+          designers have the most free reign to design a creative solution to the problem we present
+          them with. This has, and will always be, StartUp’s core.
+          <br />
+          <br />
+          <i>*Judging criteria will be different for each track</i>
+        </p>
       </div>
     )}
   </Transition>
-)
-
-
-
-
+);
 
 const IndexPage = props => {
-
   const [track, setTrack] = useState(1);
 
   return (
@@ -229,41 +250,41 @@ const IndexPage = props => {
           </p>
         </div>
 
-{/* slider */}
+        {/* slider */}
         <div className="track-grid">
           <div>
             <img src={Dude} alt="dude" />
           </div>
           <div className="track-container">
-          <div className="track-buttonContainer">
-          <button onClick={() => setTrack(1)}>Track 1</button>
-          <button onClick={() => setTrack(2)}>Track 2</button>
-          <div id="track-buttonBackground" className={ track === 1 ? "track-buttonBackground" : "track-buttonBackground track-buttonright"}></div>
-        </div>
-        <div className="track-animationContainer ">
-          {track === 1 ?
-          <>
-            <TrackOne in={true} />
-            <TrackTwo in={false} />
-          </> : 
-          <>
-            <TrackOne in={false} />
-            <TrackTwo in={true} />
-          </>}
-        </div>
+            <div className="track-buttonContainer">
+              <button onClick={() => setTrack(1)}>Track 1</button>
+              <button onClick={() => setTrack(2)}>Track 2</button>
+              <div
+                id="track-buttonBackground"
+                className={
+                  track === 1
+                    ? "track-buttonBackground"
+                    : "track-buttonBackground track-buttonright"
+                }
+              ></div>
+            </div>
+            <div className="track-animationContainer ">
+              {track === 1 ? (
+                <>
+                  <TrackOne in={true} />
+                  <TrackTwo in={false} />
+                </>
+              ) : (
+                <>
+                  <TrackOne in={false} />
+                  <TrackTwo in={true} />
+                </>
+              )}
+            </div>
           </div>
 
-          
-
-        {track === 1 ?
-          <>
-          
-          </> : 
-          <>
-        
-          </>}
-
-      </div>
+          {track === 1 ? <></> : <></>}
+        </div>
       </main>
     </Page>
   );
