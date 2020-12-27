a5c75acc06549aeb516b1ee8e3fefb0a94f7829e
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2020-12-27 00:18:28 -0500

    idk

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -23,20 +23,20 @@ import Dude from "../assets/images/dude.png";
 // import TrackOneImg from '../assets/images/track-local.png'
 // import TrackTwoImg from '../assets/images/track-entrepreneurship.png'
 // import StartupBig from "../assets/svg/StartupBig.svg";
-import one from "../assets/carousel/one.jpg";
-import two from "../assets/carousel/two.jpg";
-import three from "../assets/carousel/three.jpg";
-import four from "../assets/carousel/four.jpg";
-import five from "../assets/carousel/five.jpg";
-import six from "../assets/carousel/six.jpg";
-import seven from "../assets/carousel/seven.jpg";
-// import eight from "../assets/carousel/startup_08.jpg";
-// import nine from "../assets/carousel/startup_09.jpg";
-// import ten from "../assets/carousel/startup_10.jpg";
-import eleven from "../assets/carousel/eleven.jpg"; 
-import twelve from "../assets/carousel/twelve.jpg";
-import thirteen from "../assets/carousel/thirteen.jpg";
-import fourteen from "../assets/carousel/fourteen.jpg";
+import one from "../assets/carousel/startup_01.png";
+import two from "../assets/carousel/startup_02.png";
+import three from "../assets/carousel/startup_03.png";
+import four from "../assets/carousel/startup_04.png";
+import five from "../assets/carousel/startup_05.png";
+import six from "../assets/carousel/startup_06.png";
+import seven from "../assets/carousel/startup_07.png";
+// import eight from "../assets/carousel/startup_08.png";
+// import nine from "../assets/carousel/startup_09.png";
+// import ten from "../assets/carousel/startup_10.png";
+import eleven from "../assets/carousel/startup_11.png";
+import twelve from "../assets/carousel/startup_12.png";
+import thirteen from "../assets/carousel/startup_13.png";
+import fourteen from "../assets/carousel/startup_14.png";
 import blue from "../assets/images/blue.png";
 
 // scss
