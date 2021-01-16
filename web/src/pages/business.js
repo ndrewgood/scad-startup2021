7b71bb6b660a599e8f2e416a86caf8e102b98401
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2021-01-16 16:35:33 -0500

    testing something

---
+++ web/src/pages/business.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Page from "../templates/page";
+
+// window.onload = function() {
+//     // similar behavior as clicking on a link
+//     window.location.href = "http://stackoverflow.com";
+// }
+
+if (typeof window !== "undefined"){
+    window.onload = function() {
+        // similar behavior as clicking on a link
+        window.location.href = "https://forms.gle/5gPieSiSUY6FQtbaA";
+    }
+  }
+
+const Business = (props) => {
+
+  return (
+    <Page>
+    </Page>
+  );
+};
+
+export default Business;
