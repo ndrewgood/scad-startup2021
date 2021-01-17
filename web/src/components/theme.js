bdcfa4721fee7c5450f87272bf5c7d2838079b4d
Eric Flatt <36873954+ericflatt@users.noreply.github.com>
2021-01-17 11:40:46 -0500

    update copy

--- web/src/components/theme.js
+++ web/src/components/theme.js
@@ -55,7 +55,7 @@ const TrackTwo = ({in: inProp}) => (
         ...transitionStyles[state]
       }}>
         <h1 className="theme-trackHeader">Giving Back To<br />Your <span>Locals</span></h1>
-        <p className="theme-trackBody">Want to give back to the community and do not know how to? StartUp is here to support you. During the global pandemic, small businesses have been hit the hardest. Join Track 2 and work hands-on with a local small business in Savannah or Atlanta, and design creative solutions catered to their unique set of problems.<br/><br/><i>*Judging criteria will be different for each track</i></p>
+        <p className="theme-trackBody">Want to give back to the community but don't know how? StartUp is here to support you. During the global pandemic, small businesses have been hit the hardest. Join Track 2 and work hands-on with a minority owned local small business in Savannah or Atlanta, and design creative solutions catered to their unique set of problems.<br/><br/><i>*Judging criteria will be different for each track</i></p>
       </div>
     )}
   </Transition>
