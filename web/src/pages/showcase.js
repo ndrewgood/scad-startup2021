6972a0a37b28a199bacaf72a97fccdf641a99910
Andrew Goodridge <gandrew888@gmail.com>
2021-02-12 10:10:18 -0500

    more updates

--- web/src/pages/showcase.js
+++ web/src/pages/showcase.js
@@ -47,7 +47,7 @@ console.log(showcaseArray);
        showcaseArray.some(i => i.node.shown == true) ? 
        <div className="showcase-background">
          <div className="showcase-container container">
-           <p className="showcase-description">Welcome to the StartUp Showcase page! Here you will be able to jump from Zoom room to Zoom room, visiting and talking to the various participants of StartUp 2021 about the projects they created this week.</p>
+           <p className="showcase-description">The teams have all put together materials for you to view their work. Click the links below to view the projects!</p>
            <a className="nav-registerButton mobileNav-registerButton showcase-voteButton LASTMINUTE" target="_blank" rel="noopener noreferrer" href="https://forms.gle/Aq9sE4TDwCm8Ppgb8">Vote For Your Favorites</a>
            <div className="showcase-track-container">
              <h2><span className="blue-highlight">Entrepreneurship</span> Track</h2>
